6d763720374f0b2398eab644dd08533fe6da8bfe
piction165 <plaes165@gmail.com>
2025-03-07 14:50:51 +0900

    Update User.js

--- js/models/User.js
+++ js/models/User.js
@@ -4,8 +4,9 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String },
   password: { type: String, required: true },
-  phoneNumber: { type: String, required: true }, // 연락처 추가
-  isApproved: { type: Boolean, default: false }, // 승인 여부 (기본값: false)
+  phoneNumber: { type: String, required: true },
+  isApproved: { type: Boolean, default: false },
+  role: { type: String, enum: ["user", "admin"], default: "user" } // 사용자 역할 추가
 });
 
 module.exports = mongoose.model("User", UserSchema);
